354f0d1146f00d6601d7ccd6026fe3594dce7fa0
filipw <wojcieszyn@gmail.com>
2019-09-18 13:59:30 +0200

    first commit

---
+++ DispatchProxySample/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reflection;
+using Library;
+
+namespace DispatchProxySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var internalType = Assembly.Load("Library").GetType("Library.Greeter");
+            var greetMethod = internalType.GetMethod("Greet", BindingFlags.Public | BindingFlags.Static);
+
+            var proxy = GreetingFactory.Create();
+            Console.WriteLine(greetMethod.Invoke(null, new[] { proxy }));
+        }
+    }
+
+    public class Greeting : DispatchProxy
+    {
+        private GreetingImpl _impl;
+
+        public Greeting()
+        {
+            _impl = new GreetingImpl();
+        }
+
+        protected override object Invoke(MethodInfo targetMethod, object[] args)
+        {
+            return _impl.GetType().GetMethod(targetMethod.Name).Invoke(_impl, args);
+        }
+
+        private class GreetingImpl // : IGreeting - doesn't implement IGreeting but mimics it
+        {
+            public string Message => "hello world";
+        }
+    }
+
+    public class GreetingFactory
+    {
+        public static object Create()
+        {
+            var internalType = Assembly.Load("Library").GetType("Library.IGreeting");
+            return typeof(DispatchProxy).GetMethod(nameof(DispatchProxy.Create)).MakeGenericMethod(internalType, typeof(Greeting)).Invoke(null, null);
+        }
+    }
+}
---
+++ Library/Greeter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Library
+{
+    internal class Greeter
+    {
+        public static void Greet(IGreeting greeting)
+        {
+            Console.WriteLine(greeting.Message);
+        }
+    }
+
+    internal interface IGreeting
+    {
+        string Message { get; }
+    }
+}
